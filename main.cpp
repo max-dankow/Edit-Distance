497840594726ea3da1148e76be9ac02b0602490f
Maxim Dankovtsev <max.dankow@gmail.com>
2015-03-16 21:08:15 +0300

    All done. testes ok. LOTR ok.

--- main.cpp
+++ main.cpp
@@ -4,8 +4,10 @@
 #include <stdio.h>
 #include <algorithm>
 #include <assert.h>
+#include <random>
 
-const bool OUTPUT_MODE = 0;
+const int OUTPUT_MATCHING = 0;
+const int OUTPUT_COMANDS = 1;
 
 size_t get_index(const size_t x, const size_t y, const size_t n)
 {
@@ -37,8 +39,6 @@ void restore_way(const std::vector<long long> &data,
         size_t next_x = data[get_index(x, y, n)] % n;
         restore_way(data, dst_way, n, m, next_x, next_y);
         dst_way.push_back(std::make_pair(next_x, next_y));
-        //dst_way[0].first = 99;
-        //std::cout << next_x << ' ' << next_y << dst_way[0].first << '\n';
     }
 }
 
@@ -64,8 +64,6 @@ long long edit_distance(const std::string &s1, const std::string &s2,
         way[get_index(i, 0, n)] = get_index(i - 1, 0, n);
     }
 
-    //write_matrix(way, n, m);
-
     for (size_t j = 1; j < m; ++j)
     {
         for (size_t i = 1; i < n; ++i)
@@ -86,8 +84,7 @@ long long edit_distance(const std::string &s1, const std::string &s2,
             }
         }
     }
-    //write_matrix(distance, n, m);
-    //write_matrix(way, n, m);
+
     dst_way.clear();
     restore_way(way, dst_way, n, m, n - 1, m - 1);
     dst_way.push_back(std::make_pair(s1.size(), s2.size()));
@@ -107,8 +104,6 @@ std::vector<long long> distance(std::string s1, std::string s2)
         (*vec[0])[i] = (*vec[0])[i - 1] + 1;
     }
 
-    //write_matrix(way, n, m);
-
     for (size_t j = 1; j < s1.size() + 1; ++j)
     {
         (*vec[1])[0] = (*vec[0])[0] + 1;
@@ -120,13 +115,11 @@ std::vector<long long> distance(std::string s1, std::string s2)
             if ((*vec[1])[i] > (*vec[0])[i] + 1)
             {
                 (*vec[1])[i] = (*vec[0])[i] + 1;
-                //distance[get_index(i, j, n)] = distance[get_index(i, j - 1, n)] + 1;
             }
 
             if ((*vec[1])[i] > (*vec[0])[i - 1] + ((s1[j - 1] == s2[i - 1])? 0 : 1))
             {
                 (*vec[1])[i] = (*vec[0])[i - 1] + ((s1[j - 1] == s2[i - 1])? 0 : 1);
-                //distance[get_index(i, j, n)] = distance[get_index(i - 1, j - 1, n)] + ((s1[i - 1] == s2[j - 1])? 0 : 1);
             }
         }
 
@@ -149,15 +142,11 @@ long long edit_distance_mod(const std::string &s1, const std::string &s2,
         {
             (*it).first += offset_x;
             (*it).second += offset_y;
-
-            //std::cout << "subADDED :";
-            //std::cout << (*it).first <<' '<< (*it).second << std::endl;
             result.push_back(*it);
         }
 
         return simple_result;
     }
-    //assert(s1.size() != 0);
 
     std::ptrdiff_t middle = (s1.size()) / 2 + (s1.size()) % 2;
 
@@ -185,28 +174,8 @@ long long edit_distance_mod(const std::string &s1, const std::string &s2,
     substrings[1].first = std::string(s1, middle, s1.size() - middle);
     substrings[1].second = std::string(s2, min_index, s2.size() - min_index);
 
-    /*if (substrings[0].first.size() <= 1  && substrings[0].second.size() <= 1 &&
-        substrings[1].first.size() <= 1 && substrings[1].second.size() <= 1)
-    {
-        //std::cout << "ADDED :";
-        //std::cout << middle + offset_x <<' '<< min_index + offset_y << std::endl;
-        result.push_back(std::make_pair(middle + offset_x, min_index + offset_y));
-        return 0;
-    }*/
-
-    //if (substrings[0].first.size() > 1  || substrings[0].second.size() > 1)
-    //{
-        edit_distance_mod(substrings[0].first, substrings[0].second, result, offset_x, offset_y);
-    //}
-
-    /*std::cout << "ADDED :";
-    std::cout << middle + offset_x <<' '<< min_index + offset_y << std::endl;
-    result.push_back(std::make_pair(middle + offset_x, min_index + offset_y));
-*/
-    //if (substrings[1].first.size() > 1  || substrings[1].second.size() > 1)
-    //{
-        edit_distance_mod(substrings[1].first, substrings[1].second, result, offset_x + middle, offset_y + min_index);
-    //}
+    edit_distance_mod(substrings[0].first, substrings[0].second, result, offset_x, offset_y);
+    edit_distance_mod(substrings[1].first, substrings[1].second, result, offset_x + middle, offset_y + min_index);
 
     return left[min_index] + right[min_index];
 }
@@ -215,13 +184,12 @@ long long edit_distance_mod(const std::string &s1, const std::string &s2,
                             std::vector<std::pair<size_t, size_t> > &result)
 {
     result.assign(1, std::make_pair(0, 0));
-    long long distance_result = edit_distance_mod(/*char(1) + */s1,/* char(1) + */s2, result, 0, 0);
-    //result.push_back(std::make_pair(s1.size(), s2.size()));
+    long long distance_result = edit_distance_mod(s1, s2, result, 0, 0);
     return distance_result;
 }
 
 void show_way(const std::string &s1, const std::string &s2,
-              const std::vector<std::pair<size_t, size_t> > &way)
+              const std::vector<std::pair<size_t, size_t> > &way, const int mode)
 {
     std::string result = "";
     for (size_t i = 1; i < way.size(); ++i)
@@ -233,91 +201,179 @@ void show_way(const std::string &s1, const std::string &s2,
             else
             {
                 result += 'R';
-                std::cout << "IN " << way[i].first - 1 << " REPLACE " << s1[way[i].first - 1]
-                          << " WITH " << s2[way[i].second - 1] << '\n';
             }
         }
 
         if ((way[i].first == way[i - 1].first) && (way[i].second == way[i - 1].second + 1))
         {
             result +='I';
-            std::cout << "BEFORE " << way[i].first << " INSERT " << s2[way[i].second - 1] << '\n';
         }
 
         if ((way[i].first == way[i - 1].first + 1) && (way[i].second == way[i - 1].second))
         {
             result += 'D';
-            std::cout << "IN " << way[i].first - 1 << " DELETE " << s1[way[i].first - 1] << '\n';
         }
     }
-    std::cout << '\n';
 
-    size_t pos = 0;
-    for (size_t i = 0; i < result.size(); ++i)
+    if (mode == OUTPUT_COMANDS)
     {
-        switch (result[i])
+        for (size_t i = 1; i < way.size(); ++i)
         {
-        case 'M':
-            printf("%c", s1[pos]);
-            pos++;
-            break;
-        case 'R':
-            printf("%c", s1[pos]);
-            pos++;
-            break;
-        case 'I':
-            printf("-");
-            break;
-        case 'D':
-            printf("%c", s1[pos]);
-            pos++;
-            break;
+            if ((way[i].first == way[i - 1].first + 1) && (way[i].second == way[i - 1].second + 1))
+            {
+                if  (!((way[i].second > 0) && (way[i].first > 0) && (s1[way[i].first - 1] == s2[way[i].second - 1])))
+                {
+                    std::cout << "IN " << way[i].first - 1 << " REPLACE " << s1[way[i].first - 1]
+                              << " WITH " << s2[way[i].second - 1] << '\n';
+                }
+            }
+
+            if ((way[i].first == way[i - 1].first) && (way[i].second == way[i - 1].second + 1))
+            {
+                std::cout << "BEFORE " << way[i].first << " INSERT " << s2[way[i].second - 1] << '\n';
+            }
+
+            if ((way[i].first == way[i - 1].first + 1) && (way[i].second == way[i - 1].second))
+            {
+                std::cout << "IN " << way[i].first - 1 << " DELETE " << s1[way[i].first - 1] << '\n';
+            }
         }
+
+        std::cout << '\n';
     }
 
-    std::cout << '\n' << result << '\n';
-
-    pos = 0;
-    for (size_t i = 0; i < result.size(); ++i)
+    if (mode == OUTPUT_MATCHING)
     {
-        switch (result[i])
+        size_t pos = 0;
+        for (size_t i = 0; i < result.size(); ++i)
         {
-        case 'M':
-            printf("%c", s2[pos]);
-            pos++;
-            break;
-        case 'R':
-            printf("%c", s2[pos]);
-            pos++;
-            break;
-        case 'D':
-            printf("-");
-            break;
-        case 'I':
-            printf("%c", s2[pos]);
-            pos++;
-            break;
+            switch (result[i])
+            {
+            case 'M':
+                std::cout << s1[pos];
+                pos++;
+                break;
+            case 'R':
+                std::cout << s1[pos];
+                pos++;
+                break;
+            case 'I':
+                printf("-");
+                break;
+            case 'D':
+                std::cout << s1[pos];
+                pos++;
+                break;
+            }
         }
+
+        std::cout << '\n' << result << '\n';
+
+        pos = 0;
+        for (size_t i = 0; i < result.size(); ++i)
+        {
+            switch (result[i])
+            {
+            case 'M':
+                std::cout << s2[pos];
+                pos++;
+                break;
+            case 'R':
+                std::cout << s2[pos];
+                pos++;
+                break;
+            case 'D':
+                printf("-");
+                break;
+            case 'I':
+                std::cout << s2[pos];
+                pos++;
+                break;
+            }
+        }
+        std::cout << '\n';
     }
-    std::cout << '\n';
+}
+
+std::default_random_engine generator;
+
+std::string generate_string(const size_t max_length, std::string &str)
+{
+    std::uniform_int_distribution<char> ValuesChar('a', 'z');
+    std::uniform_int_distribution<size_t> ValuesStrLength(0, max_length);
+    int lengthStr = ValuesStrLength(generator);
+    str.clear();
+
+    for (int i = 0; i <= lengthStr; i++)
+        str += char(ValuesChar(generator));
+    return str;
+}
+
+void test_hirschberg_algorithm(size_t test_number)
+{
+    std::string s1;
+    std::string s2;
+
+    for (size_t i = 0; i < test_number; ++i)
+    {
+        s1 = generate_string(20, s1);
+        s2 = generate_string(20, s2);
+        std::vector<std::pair<size_t, size_t> > way;
+
+        std::cout << s1 << " -> " << s2 << '\n';
+
+        std::cout << "\nORIGINAL \n";
+        int result_origin = edit_distance(s1, s2, way);
+        show_way(s1, s2, way, OUTPUT_MATCHING);
+
+        std::cout << "\nHIRSCHBERG \n";
+        int result_mode = edit_distance_mod(s1, s2, way);
+        show_way(s1, s2, way, OUTPUT_MATCHING);
+
+        if (result_origin != result_mode)
+        {
+            perror("Error.");
+            return;
+        }
+        std::cout << '\n';
+    }
+
+    std::cout << "Tests OK.\n";
+}
+
+void read_file_into_str(const char *file_name, std::string &str)
+{
+    FILE *open_file = fopen((char*) file_name, "r");
+
+    if (open_file == NULL)
+    {
+        perror("Can't open file.");
+        return;
+    }
+
+    char ch;
+
+    while ((ch = fgetc(open_file)) != EOF)
+    {
+        str += ch;
+    }
+
+    fclose(open_file);
 }
 
 int main()
 {
-    std::string s1 = "l", s2 = "lil";
+    test_hirschberg_algorithm(10);
+    std::string s1;
+    std::string s2;
+    read_file_into_str("lord.txt", s1);
+    read_file_into_str("lord_changed.txt", s2);
+
+    std::cout << "\nHIRSCHBERG FOR LOTR ";
+
     std::vector<std::pair<size_t, size_t> > way;
-
-    std::cout << "\nORIGINAL ";
-
-    std::cout << edit_distance(s1, s2, way) << "\n\n";
-    show_way(s1, s2, way);
-    //distance(s1, s2);
-
-    std::cout << "\nHIRSCHBERG ";
-
-    //std::vector<std::pair<size_t, size_t> > way;
     std::cout << edit_distance_mod(s1, s2, way) << "\n\n";
-    show_way(s1, s2, way);
+    show_way(s1, s2, way, OUTPUT_COMANDS);
 
     return 0;
 }
